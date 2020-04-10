08358233590e92d5a23fd60f3b026624b2d23fa5
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-04-10 11:31:52 -0500

    New helper function

--- helper_functions.c
+++ helper_functions.c
@@ -19,7 +19,25 @@ int _strcmp(char *s1, char *s2)
 	else
 		return (s1[i] - s2[i]);
 }
+/**
+ * _strcmp - Compare strings.
+ * @s1: String 1.
+ * @s2: String 2.
+ * Return: number.
+ */
+int _strcmpn(char *s1, char *s2, int n)
+{
+	int i = 0;
 
+	while ((s1[i] == s2[i] && s1[i] && s2[i]) && i < n - 1)
+	{
+		i++;
+	}
+	if ((s1[i] == s2[i]) && i == n - 1)
+		return (0);
+	else
+		return (s1[i] - s2[i]);
+}
 /**
  * _puts - print string.
  * @str: string.
